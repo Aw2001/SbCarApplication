5aa67829ae0a4169788bd650378ce3b0e04363d4
Eva Fall <awafall82817@gmail.com>
2024-03-29 11:39:06 +0000

    first_page

---
+++ src/test/java/esp/dgi/sbcarapplication/SbCarApplicationTests.java
@@ -0,0 +1,13 @@
+package esp.dgi.sbcarapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbCarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
